6ee24379ee368081fbc03e2ae3f796be7e6841f1
¨Hernan <hernandaco21@hotmail.com>
2024-09-26 19:47:45 -0500

    Se crea el archivo de operadores aritmeticos en python

---
+++ operadoreProject/OperadoresAritmeticos.py
@@ -0,0 +1,19 @@
+#Ejemplo operadores Aritmeticos
+#numero1:int = 18
+#numero2:int = 12
+numero1 = int (input("Ingrese el numero 1: "))
+numero2 = int (input("Ingrese el numero 2: "))
+
+suma = numero1 + numero2
+resta = numero1 - numero2
+division = numero1 / numero2
+multiplicacion = numero1 * numero2
+modulo = numero1 % numero2
+divicionEntera = numero1 // numero2
+
+print(f"La suma de {numero1} y {numero2} es : {suma} ")
+print(f"La resta de {numero1} y {numero2} es : {resta} ")
+print(f"La division de {numero1} y {numero2} es : {division} ")
+print(f"La multiplicacion de {numero1} y {numero2} es : {multiplicacion} ")
+print(f"El modulo de {numero1} y {numero2} es : {modulo} ",)
+print(f"La divion entera de {numero1} y {numero2} es : {divicionEntera} ")
