74c79f36238e629f02c1bad42c4acd51bb8be000
¨Hernan <hernandaco21@hotmail.com>
2024-09-26 20:55:02 -0500

    Se crea archivo operadores de comparacion en pythom

---
+++ operadoreProject/OperadoresDeComparacion.py
@@ -0,0 +1,11 @@
+#Ejemplo con Operadores de Comparacion
+
+m = int(input("Ingrese el valor de m: "))
+n = int(input("Ingrese el valor de n: "))
+
+print(m == n) # Igualdad
+print(m > n) # Mayor que >
+print(m < n) #menor que <
+print(m >= n) #Mayor igual que >=
+print(m <= n) # Menor igual que <=
+print(m != n) # No igual o diferente !
